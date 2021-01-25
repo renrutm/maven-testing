8cee83dd2efca0b9b71cefea0de11d6a09bb9398
michael turner <michael.turner.developer@gmail.com>
2021-01-25 16:15:40 +0100

    testing with spock and jUnit5

---
+++ src/test/java/com/mkturner/maventesting/HelloWorldIT.java
@@ -0,0 +1,11 @@
+package com.mkturner.maventesting;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldIT {
+
+    @Test
+    public void myIntegrationTest() {
+        System.out.println("Test");
+    }
+}
